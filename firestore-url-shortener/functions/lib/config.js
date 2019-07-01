911bf3975e6d9f8fd3b1aeb2fa1c0f0c1fe5d901
Chris Bianca <chris@csfrequency.com>
2019-07-01 17:29:31 +0100

    Quality review of the Firestore URL Shortener mod (#63)
    * Quality review of the Firestore URL Shortener mod
    
    * Tweak export format
    
    * Comment description field as cli fails install
    
    * Further tweaks to code structure
    
    * Move configuration to a separate file

---
+++ firestore-url-shortener/functions/lib/config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    bitlyAccessToken: process.env.BITLY_ACCESS_TOKEN,
+    collectionPath: process.env.COLLECTION_PATH,
+    location: process.env.LOCATION,
+    shortUrlFieldName: process.env.SHORT_URL_FIELD_NAME,
+    urlFieldName: process.env.URL_FIELD_NAME,
+};
---
+++ firestore-url-shortener/functions/src/config.ts
@@ -0,0 +1,7 @@
+export default {
+  bitlyAccessToken: process.env.BITLY_ACCESS_TOKEN,
+  collectionPath: process.env.COLLECTION_PATH,
+  location: process.env.LOCATION,
+  shortUrlFieldName: process.env.SHORT_URL_FIELD_NAME,
+  urlFieldName: process.env.URL_FIELD_NAME,
+};
