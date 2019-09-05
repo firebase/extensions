ace9e81705f2bc7df9163bcd8197192138666465
Michael Nguyen <nguymichael@google.com>
2019-09-05 11:48:50 -0700

    robust

--- postponed/firestore-presence-detection/functions/lib/logs.js
+++ postponed/firestore-presence-detection/functions/lib/logs.js
@@ -4,23 +4,30 @@ const config_1 = require("./config");
 exports.error = (err) => {
     console.error('Error executing mod: ', err);
 };
+exports.nonFatalError = (err) => {
+    console.error('Non-fatal error encountered (continuing execution): ', err);
+};
 exports.start = () => {
     console.log('Started mod execution with config \n', config_1.default);
 };
-exports.handleUpsert = (path, payload) => {
-    console.log("Upserting data: " + JSON.stringify(payload) + " at path: " + path);
+exports.handleCreate = (sessionID) => {
+    console.log("Detected new connection. Creating new path: " + sessionID);
 };
-exports.handleDelete = (path) => {
-    console.log(`Deleting user connection at: ${path}`);
+exports.handleUpdate = (sessionID, payload) => {
+    console.log("Detected metadata update for session: " + sessionID + " with payload: " + JSON.stringify(payload));
 };
-exports.getSessionInfo = (path) => {
-    console.log(`Obtaining sessionID and userID from path: ${path}`);
+exports.handleDelete = (sessionID) => {
+    console.log(`Detected disconnect. Removing connection: ${sessionID}`);
 };
-exports.logTimestampComparison = (currentTimestamp, operationTimestamp, userID, sessionID) => {
-    console.log(`Comparing timestamps of user: ${userID}, session: ${sessionID}. Current Timestamp: ${currentTimestamp}, Operation Timestamp: ${operationTimestamp}`);
+exports.getSessionInfo = (sessionID, userID) => {
+    console.log(`Mod executing for user: ${userID}, connection: ${sessionID}`);
+};
+exports.logStaleTimestamp = (currentTimestamp, operationTimestamp, userID, sessionID) => {
+    console.log(`Timestamp of current operation is older than timestamp at destination. Refusing to commit change.` +
+        `(user: ${userID}, session: ${sessionID} | Destination Timestamp: ${currentTimestamp}, Operation Timestamp: ${operationTimestamp})`);
 };
 exports.logFirestoreUpsert = (payload) => {
-    console.log("Updating Firestore document with payload: " + JSON.stringify(payload));
+    console.log("Firestore document successfully updated with payload: " + JSON.stringify(payload));
 };
 exports.success = () => {
     console.log("User presence extension successfully executed.");
@@ -29,6 +36,6 @@ exports.createDocument = (document, collection) => {
     console.log(`Creating document ${document} at Collection ${collection} `);
 };
 exports.logRetry = (err) => {
-    console.error('Error commiting RTDB changes, retrying. Error: ', err);
+    console.error('Error commiting changes to Firestore, retrying. Error: ', err);
 };
 //# sourceMappingURL=logs.js.map
