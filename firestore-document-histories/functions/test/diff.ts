70bc5bc75b01ef722d06811055351e89c66e1565
Fred Zhang <fredzqm@google.com>
2019-09-11 14:50:32 -0700

    Add license file and small docs change

--- firestore-document-histories/functions/test/diff.ts
+++ firestore-document-histories/functions/test/diff.ts
@@ -1,7 +1,23 @@
-import { expect } from "chai";
-import { suite, test } from "mocha-typescript";
-import { firestore } from "firebase-admin";
-import { computeDiff } from "../src/diff"
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {expect} from "chai";
+import {suite, test} from "mocha-typescript";
+import {firestore} from "firebase-admin";
+import {computeDiff} from "../src/diff"
 
 @suite
 class DifferTest {
@@ -76,156 +92,156 @@ class DifferTest {
 
   @test "nested map computeDiffed to be empty"() {
     expect(computeDiff(
-      {
-        x: {
-          b: {
-            z: "ok"
+        {
+          x: {
+            b: {
+              z: "ok"
+            }
           }
-        }
-      },
-      {
-        x: {
-          b: {
-            z: "ok"
-          }
-        }
-      })).to.be.undefined;
-
-    expect(computeDiff(
-      {
-        x: {
-          a: 1,
-          b: {
-            z: "ok"
-          }
-        }
-      },
-      {
-        x: {
-          a: 1,
-          b: {
-            z: "ok"
-          },
-          c: 0
         },
-        y: [1]
-      })).to.be.undefined;
+        {
+          x: {
+            b: {
+              z: "ok"
+            }
+          }
+        })).to.be.undefined;
 
     expect(computeDiff(
-      {
-        x: {
-          a: 1,
-          b: 2
-        }
-      },
-      {
-        x: {
-          a: 1,
-          b: 2
-        }
-      })).to.be.undefined;
+        {
+          x: {
+            a: 1,
+            b: {
+              z: "ok"
+            }
+          }
+        },
+        {
+          x: {
+            a: 1,
+            b: {
+              z: "ok"
+            },
+            c: 0
+          },
+          y: [1]
+        })).to.be.undefined;
 
     expect(computeDiff(
-      {
-        x: {
-          a: 1
-        }
-      },
-      {
-        x: {
-          a: 1,
-          b: 2
-        }
-      })).to.be.undefined;
+        {
+          x: {
+            a: 1,
+            b: 2
+          }
+        },
+        {
+          x: {
+            a: 1,
+            b: 2
+          }
+        })).to.be.undefined;
+
+    expect(computeDiff(
+        {
+          x: {
+            a: 1
+          }
+        },
+        {
+          x: {
+            a: 1,
+            b: 2
+          }
+        })).to.be.undefined;
   }
 
   @test "nested map computeDiffed to be not empty"() {
     expect(computeDiff(
-      {
-        x: {
-          b: {
-            z: "ok"
+        {
+          x: {
+            b: {
+              z: "ok"
+            }
           }
+        },
+        {
+          y: []
+        })).deep.equal({
+      x: {
+        b: {
+          z: "ok"
         }
-      },
-      {
-        y: []
-      })).deep.equal({
-        x: {
-          b: {
-            z: "ok"
-          }
-        }
-      });
+      }
+    });
 
     expect(computeDiff(
-      {
-        x: {
-          b: {
-            z: "ok"
+        {
+          x: {
+            b: {
+              z: "ok"
+            }
           }
-        }
-      },
-      {
-        x: {
-          b: {
-            z: 1
+        },
+        {
+          x: {
+            b: {
+              z: 1
+            }
           }
+        })).deep.equal({
+      x: {
+        b: {
+          z: "ok"
         }
-      })).deep.equal({
-        x: {
-          b: {
-            z: "ok"
-          }
-        }
-      });
+      }
+    });
 
-      expect(computeDiff(
-      {
-        x: {
-          a: 1,
-          b: 1,
-        }
-      },
-      {
-        x: {
-          b: 1,
-        }
-      })).deep.equal(
-      {
-        x: {
-          a: 1
-        }
-      });
+    expect(computeDiff(
+        {
+          x: {
+            a: 1,
+            b: 1,
+          }
+        },
+        {
+          x: {
+            b: 1,
+          }
+        })).deep.equal(
+        {
+          x: {
+            a: 1
+          }
+        });
 
-      expect(computeDiff(
-      {
-        x: {
-          a: 1,
-          b: 2
+    expect(computeDiff(
+        {
+          x: {
+            a: 1,
+            b: 2
+          },
+          y: {
+            a: 1,
+            b: 2
+          }
         },
-        y: {
-          a: 1,
-          b: 2
-        }
-      },
-      {
-        x: {
-          a: 1,
-          b: 2
-        },
-        y: {
-          a: 1
-        },
-        z: {
-          a: 1,
-        }
-      })).deep.equal(
-      {
-        y: {
-          b: 2
-        }
-      });
+        {
+          x: {
+            a: 1,
+            b: 2
+          },
+          y: {
+            a: 1
+          },
+          z: {
+            a: 1,
+          }
+        })).deep.equal(
+        {
+          y: {
+            b: 2
+          }
+        });
   }
 
   @test "array are identical"() {
@@ -249,5 +265,6 @@ class DifferTest {
     expect(computeDiff([1, "a", "a"], [1])).deep.equal([1, "a", "a"]);
     expect(computeDiff([1, 1, "a", "a"], [1])).deep.equal([1, 1, "a", "a"]);
   }
-    ////
+
+  ////
 }
