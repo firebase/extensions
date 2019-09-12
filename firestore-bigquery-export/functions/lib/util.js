05d85179b816439597e72e7a70d2e88d05d56f26
Jan Wyszynski <wyszynski@google.com>
2019-09-11 17:16:08 -0700

    update imports where needed (#290)

---
+++ firestore-bigquery-export/functions/lib/util.js
@@ -0,0 +1,44 @@
+"use strict";
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const firestore_bigquery_change_tracker_1 = require("@firebaseextensions/firestore-bigquery-change-tracker");
+function getChangeType(change) {
+    if (!change.after.exists) {
+        return firestore_bigquery_change_tracker_1.ChangeType.DELETE;
+    }
+    if (!change.before.exists) {
+        return firestore_bigquery_change_tracker_1.ChangeType.CREATE;
+    }
+    return firestore_bigquery_change_tracker_1.ChangeType.UPDATE;
+}
+exports.getChangeType = getChangeType;
+function getTimestamp(context, change) {
+    const changeType = getChangeType((change));
+    switch (changeType) {
+        case firestore_bigquery_change_tracker_1.ChangeType.CREATE:
+            return change.after.updateTime.toDate();
+        case firestore_bigquery_change_tracker_1.ChangeType.DELETE:
+            // Due to an internal bug (129264426), before.update_time is actually the commit timestamp.
+            return new Date(change.before.updateTime.toDate().getTime() + 1);
+        case firestore_bigquery_change_tracker_1.ChangeType.UPDATE:
+            return change.after.updateTime.toDate();
+        default: {
+            throw new Error(`Invalid change type: ${changeType}`);
+        }
+    }
+}
+exports.getTimestamp = getTimestamp;
