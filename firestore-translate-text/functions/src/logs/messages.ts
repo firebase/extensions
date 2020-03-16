bffe681416dc6bc1285c2ad42219487070de1350
Joe Hanley <joehanley@google.com>
2020-03-16 08:16:26 -0700

    prettier

--- firestore-translate-text/functions/src/logs/messages.ts
+++ firestore-translate-text/functions/src/logs/messages.ts
@@ -17,7 +17,7 @@ export const messages = {
     "The `Input` and `Output` field names must be different for this extension to function correctly",
   init: (config = {}) => [
     "Initializing extension with the parameter values",
-    config
+    config,
   ],
   inputFieldNameIsOutputPath: () =>
     "The `Input` field name must not be the same as an `Output` path for this extension to function correctly",
