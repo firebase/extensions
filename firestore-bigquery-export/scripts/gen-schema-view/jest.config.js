f83ba12f2a741fd9884ed40d7df07106e5d7ce25
Jan Wyszynski <wyszynski@google.com>
2020-04-10 18:26:55 -0700

    commit jest configs for firestore-bigquery-export packages

---
+++ firestore-bigquery-export/scripts/gen-schema-view/jest.config.js
@@ -0,0 +1,14 @@
+const packageJson = require("./package.json");
+
+module.exports = {
+  name: packageJson.name,
+  displayName: packageJson.name,
+  rootDir: "./",
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.json",
+    },
+  },
+  preset: "ts-jest",
+  testMatch: ["<rootDir>/src/test/bigquery/*.spec.ts", "<rootDir>/src/test/schema-loader-utils/*.spec.ts"],
+};
