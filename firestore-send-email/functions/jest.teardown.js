ec34cad009859603d7c1dbdeb1296cd98f28050c
dackers86 <ackers86@hotmail.com>
2020-04-14 10:41:19 +0100

    refactor(delete-user-data): additional updates

---
+++ firestore-send-email/functions/jest.teardown.js
@@ -0,0 +1,9 @@
+module.exports = async function() {
+  delete process.env.LOCATION;
+  delete process.env.MAIL_COLLECTION;
+  delete process.env.SMTP_CONNECTION_URI;
+  delete process.env.DEFAULT_FROM;
+  delete process.env.DEFAULT_REPLY_TO;
+  delete process.env.USERS_COLLECTION;
+  delete process.env.TEMPLATES_COLLECTION;
+};
