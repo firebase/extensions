cc974832320edeb8ed0b83827535a4d475874f96
Russell Wheatley <russellwheatley85@gmail.com>
2020-04-22 19:14:10 +0100

    fix(storage-resize-image): fix sharp to v0.23 (#278)

--- storage-resize-images/functions/lib/index.js
+++ storage-resize-images/functions/lib/index.js
@@ -38,7 +38,10 @@ const util_1 = require("./util");
 // Initialize the Firebase Admin SDK
 admin.initializeApp();
 logs.init();
-
+/**
+ * When an image is uploaded in the Storage bucket, we generate a resized image automatically using
+ * the Sharp image converting library.
+ */
 exports.generateResizedImage = functions.storage.object().onFinalize((object) => __awaiter(void 0, void 0, void 0, function* () {
     logs.start();
     const { contentType } = object; // This is the image MIME type
