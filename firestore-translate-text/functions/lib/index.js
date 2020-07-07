e5d285c8f5986bdf461b6803d33e3ea10e7812c1
Daniel Young Lee <taeold@gmail.com>
2020-07-07 14:39:53 -0700

    chore(*): Build js for July 7 release.

--- firestore-translate-text/functions/lib/index.js
+++ firestore-translate-text/functions/lib/index.js
@@ -24,6 +24,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.fstranslate = void 0;
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const translate_1 = require("@google-cloud/translate");
