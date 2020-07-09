b2bb10799f50b7933d891fdf2880b3c387d7a3c8
jhuleatt <jhuleatt@gmail.com>
2020-07-09 09:51:45 -0700

    reran npm install at root

--- storage-resize-images/functions/lib/index.js
+++ storage-resize-images/functions/lib/index.js
@@ -24,7 +24,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateResizedImage = void 0;
 const admin = require("firebase-admin");
 const fs = require("fs");
 const functions = require("firebase-functions");
