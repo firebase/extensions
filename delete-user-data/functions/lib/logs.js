d0fe2a25b47f84386bd80dbf40b54555c454f1f6
Darren Ackers <ackers86@hotmail.com>
2020-08-24 17:34:35 +0100

    chore(delete-user-data): migration to Node.js v10 (#431)

--- delete-user-data/functions/lib/logs.js
+++ delete-user-data/functions/lib/logs.js
@@ -16,70 +16,71 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.storagePathError = exports.storagePath404 = exports.storagePathDeleting = exports.storagePathDeleted = exports.storageNotConfigured = exports.storageDeleting = exports.storageDeleted = exports.start = exports.rtdbPathError = exports.rtdbPathDeleting = exports.rtdbNotConfigured = exports.rtdbPathDeleted = exports.rtdbDeleting = exports.rtdbDeleted = exports.init = exports.firestorePathError = exports.firestorePathDeleting = exports.firestorePathDeleted = exports.firestoreNotConfigured = exports.firestoreDeleting = exports.firestoreDeleted = exports.complete = void 0;
+const firebase_functions_1 = require("firebase-functions");
 const config_1 = require("./config");
 exports.complete = (uid) => {
-    console.log(`Successfully removed data for user: ${uid}`);
+    firebase_functions_1.logger.log(`Successfully removed data for user: ${uid}`);
 };
 exports.firestoreDeleted = () => {
-    console.log("Finished deleting user data from Cloud Firestore");
+    firebase_functions_1.logger.log("Finished deleting user data from Cloud Firestore");
 };
 exports.firestoreDeleting = () => {
-    console.log("Deleting user data from Cloud Firestore");
+    firebase_functions_1.logger.log("Deleting user data from Cloud Firestore");
 };
 exports.firestoreNotConfigured = () => {
-    console.log("Cloud Firestore paths are not configured, skipping");
+    firebase_functions_1.logger.log("Cloud Firestore paths are not configured, skipping");
 };
 exports.firestorePathDeleted = (path, recursive) => {
-    console.log(`Deleted: '${path}' from Cloud Firestore ${recursive ? "with recursive delete" : ""}`);
+    firebase_functions_1.logger.log(`Deleted: '${path}' from Cloud Firestore ${recursive ? "with recursive delete" : ""}`);
 };
 exports.firestorePathDeleting = (path, recursive) => {
-    console.log(`Deleting: '${path}' from Cloud Firestore ${recursive ? "with recursive delete" : ""}`);
+    firebase_functions_1.logger.log(`Deleting: '${path}' from Cloud Firestore ${recursive ? "with recursive delete" : ""}`);
 };
 exports.firestorePathError = (path, err) => {
-    console.error(`Error when deleting: '${path}' from Cloud Firestore`, err);
+    firebase_functions_1.logger.error(`Error when deleting: '${path}' from Cloud Firestore`, err);
 };
 exports.init = () => {
-    console.log("Initializing extension with configuration", config_1.default);
+    firebase_functions_1.logger.log("Initializing extension with configuration", config_1.default);
 };
 exports.rtdbDeleted = () => {
-    console.log("Finished deleting user data from the Realtime Database");
+    firebase_functions_1.logger.log("Finished deleting user data from the Realtime Database");
 };
 exports.rtdbDeleting = () => {
-    console.log("Deleting user data from the Realtime Database");
+    firebase_functions_1.logger.log("Deleting user data from the Realtime Database");
 };
 exports.rtdbPathDeleted = (path) => {
-    console.log(`Deleted: '${path}' from the Realtime Database`);
+    firebase_functions_1.logger.log(`Deleted: '${path}' from the Realtime Database`);
 };
 exports.rtdbNotConfigured = () => {
-    console.log("Realtime Database paths are not configured, skipping");
+    firebase_functions_1.logger.log("Realtime Database paths are not configured, skipping");
 };
 exports.rtdbPathDeleting = (path) => {
-    console.log(`Deleting: '${path}' from the Realtime Database`);
+    firebase_functions_1.logger.log(`Deleting: '${path}' from the Realtime Database`);
 };
 exports.rtdbPathError = (path, err) => {
-    console.error(`Error when deleting: '${path}' from the Realtime Database`, err);
+    firebase_functions_1.logger.error(`Error when deleting: '${path}' from the Realtime Database`, err);
 };
 exports.start = () => {
-    console.log("Started extension execution with configuration", config_1.default);
+    firebase_functions_1.logger.log("Started extension execution with configuration", config_1.default);
 };
 exports.storageDeleted = () => {
-    console.log("Finished deleting user data from Cloud Storage");
+    firebase_functions_1.logger.log("Finished deleting user data from Cloud Storage");
 };
 exports.storageDeleting = () => {
-    console.log("Deleting user data from Cloud Storage");
+    firebase_functions_1.logger.log("Deleting user data from Cloud Storage");
 };
 exports.storageNotConfigured = () => {
-    console.log("Cloud Storage paths are not configured, skipping");
+    firebase_functions_1.logger.log("Cloud Storage paths are not configured, skipping");
 };
 exports.storagePathDeleted = (path) => {
-    console.log(`Deleted: '${path}' from Cloud Storage`);
+    firebase_functions_1.logger.log(`Deleted: '${path}' from Cloud Storage`);
 };
 exports.storagePathDeleting = (path) => {
-    console.log(`Deleting: '${path}' from Cloud Storage`);
+    firebase_functions_1.logger.log(`Deleting: '${path}' from Cloud Storage`);
 };
 exports.storagePath404 = (path) => {
-    console.log(`File: '${path}' does not exist in Cloud Storage, skipping`);
+    firebase_functions_1.logger.log(`File: '${path}' does not exist in Cloud Storage, skipping`);
 };
 exports.storagePathError = (path, err) => {
-    console.error(`Error deleting: '${path}' from Cloud Storage`, err);
+    firebase_functions_1.logger.error(`Error deleting: '${path}' from Cloud Storage`, err);
 };
