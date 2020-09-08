e4704fb4dd2c4ca8aa3c20baf9d234baa59c73b4
Michael Bleigh <bleigh@google.com>
2020-09-08 11:44:10 -0700

    feat(firestore-send-email): adds support for Handlebars partials (#419)
    Co-authored-by: russellwheatley <russellwheatley85@gmail.com>

--- firestore-send-email/functions/src/templates.ts
+++ firestore-send-email/functions/src/templates.ts
@@ -14,7 +14,13 @@
  * limitations under the License.
  */
 
-import { compile } from "handlebars";
+import { create } from "handlebars";
+
+const subjHandlebars = create();
+const htmlHandlebars = create();
+const textHandlebars = create();
+const ampHandlebars = create();
+
 import { logger } from "firebase-functions";
 
 interface TemplateGroup {
@@ -22,6 +28,15 @@ interface TemplateGroup {
   html?: HandlebarsTemplateDelegate;
   text?: HandlebarsTemplateDelegate;
   amp?: HandlebarsTemplateDelegate;
+}
+
+interface TemplateData {
+  name: string;
+  subject?: string;
+  html?: string;
+  text?: string;
+  amp?: string;
+  partial?: boolean;
 }
 
 export default class Templates {
@@ -49,23 +64,44 @@ export default class Templates {
   }
 
   private updateTemplates(snap: FirebaseFirestore.QuerySnapshot) {
-    snap.docs.forEach((doc) => {
-      const data = doc.data();
-      const templates: TemplateGroup = {};
-      if (data.subject) {
-        templates.subject = compile(data.subject, { noEscape: true });
+    const all: TemplateData[] = snap.docs.map((doc) =>
+      Object.assign({ name: doc.id }, doc.data())
+    );
+    const partials = all.filter((t) => t.partial);
+    const templates = all.filter((t) => !t.partial);
+
+    partials.forEach((p) => {
+      if (p.subject) {
+        subjHandlebars.registerPartial(p.name, p.subject);
       }
-      if (data.html) {
-        templates.html = compile(data.html);
+      if (p.html) {
+        htmlHandlebars.registerPartial(p.name, p.html);
       }
-      if (data.text) {
-        templates.text = compile(data.text, { noEscape: true });
+      if (p.text) {
+        textHandlebars.registerPartial(p.name, p.text);
       }
-      if (data.amp) {
-        templates.amp = compile(data.amp);
+      if (p.amp) {
+        ampHandlebars.registerPartial(p.name, p.amp);
       }
-      this.templateMap[doc.id] = templates;
-      logger.log(`loaded template '${doc.id}'`);
+      console.log(`registered partial '${p.name}'`);
+    });
+
+    templates.forEach((t) => {
+      const tgroup: TemplateGroup = {};
+      if (t.subject) {
+        tgroup.subject = subjHandlebars.compile(t.subject, { noEscape: true });
+      }
+      if (t.html) {
+        tgroup.html = htmlHandlebars.compile(t.html);
+      }
+      if (t.text) {
+        tgroup.text = textHandlebars.compile(t.text, { noEscape: true });
+      }
+      if (t.amp) {
+        tgroup.amp = ampHandlebars.compile(t.amp);
+      }
+      this.templateMap[t.name] = tgroup;
+      logger.log(`loaded template '${t.name}'`);
     });
     this.ready = true;
     this.waits.forEach((wait) => wait());
