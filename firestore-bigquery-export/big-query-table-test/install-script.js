8630134b61db61e0f004eba9a37cd834bc228498
russellwheatley <russellwheatley85@gmail.com>
2020-10-16 15:37:00 +0100

    test(firestore-bigquery-export): test scripts

---
+++ firestore-bigquery-export/big-query-table-test/install-script.js
@@ -0,0 +1,48 @@
+const admin = require("firebase-admin");
+const faker = require("faker");
+
+const [projectId, collection] = process.argv.slice(2);
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: `https://${projectId}.firebaseio.com`,
+});
+
+function data() {
+  return {
+    prop1: [faker.random.word(), faker.random.word(), faker.random.word()],
+    prop2: faker.random.word(),
+    prop3: faker.random.number(),
+    prop4: faker.random.boolean(),
+    prop5: new admin.firestore.GeoPoint(
+      faker.random.number(90),
+      faker.random.number(90)
+    ),
+    prop6: admin.firestore.Timestamp.now(),
+    prop7: admin
+      .firestore()
+      .collection(collection)
+      .doc(faker.random.word()),  
+  };
+}
+
+const promises = [];
+
+Array.from({ length: 9 }).forEach(() => {
+  const document = admin
+    .firestore()
+    .collection(collection)
+    .add(data());
+
+  promises.push(document);
+});
+
+const update = admin
+  .firestore()
+  .collection(collection)
+  .doc("update_test_doc")
+  .set(data());
+
+  promises.push(update);
+
+return Promise.all(promises);
