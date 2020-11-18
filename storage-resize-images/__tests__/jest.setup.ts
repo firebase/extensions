8cfd8e9ee3bf8cc0f1d6624c8998bd0ecbe7c9cb
russellwheatley <russellwheatley85@gmail.com>
2020-11-18 11:11:30 +0000

    test(storage-resize-images): main module

--- storage-resize-images/__tests__/jest.setup.ts
+++ storage-resize-images/__tests__/jest.setup.ts
@@ -1,2 +1,7 @@
+import {
+  mockGenerateResizedImage
+} from "./mocks/generateResizedImage";
+
 global.config = () => require("../functions/src/config").default;
 global.deleteImage = () => require("../functions/src/config").deleteImage;
+global.mockGenerateResizedImage = mockGenerateResizedImage;
