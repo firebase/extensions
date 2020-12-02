7dad8d68103c5cf4fa1c197aa9cb1b09aafef1f6
russellwheatley <russellwheatley85@gmail.com>
2020-12-02 11:19:46 +0000

    chore(firestore-counter): update readme & swift.package

--- firestore-counter/clients/ios/Package.swift
+++ firestore-counter/clients/ios/Package.swift
@@ -4,33 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "FirestoreCounter",
-    platforms: [
-      .iOS(.v10)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "FirestoreCounter",
-            targets: ["FirestoreCounter"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Firebase",
-                 url: "https://github.com/firebase/firebase-ios-sdk.git",
-                 .branch("7.0-spm-beta"))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "FirestoreCounter",
-            dependencies: [
-                .product(name: "FirebaseFirestore", package: "Firebase")
-            ]),
-        .testTarget(
-            name: "FirestoreCounterTests",
-            dependencies: ["FirestoreCounter"]),
-    ]
+  name: "FirestoreCounter",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "FirestoreCounter",
+      targets: ["FirestoreCounter"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+    .package(name: "Firebase",
+      url: "https://github.com/firebase/firebase-ios-sdk.git",
+      .branch("7.0.0"))
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "FirestoreCounter",
+      dependencies: [
+        .product(name: "FirebaseFirestore", package: "Firebase")
+      ]),
+  ]
 )
