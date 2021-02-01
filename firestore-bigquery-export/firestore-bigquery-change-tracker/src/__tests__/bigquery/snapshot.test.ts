4d1ec3615df468e1828ec98137461cf291f65fd8
russellwheatley <russellwheatley85@gmail.com>
2021-02-01 15:20:01 +0000

    feat(firestore-bigquery-export): table partitioning

--- firestore-bigquery-export/firestore-bigquery-change-tracker/src/__tests__/bigquery/snapshot.test.ts
+++ firestore-bigquery-export/firestore-bigquery-change-tracker/src/__tests__/bigquery/snapshot.test.ts
@@ -40,6 +40,7 @@ const trackerInstance = new FirestoreBigQueryEventHistoryTracker({
   datasetId: "id",
   datasetLocation: undefined,
   tableId: "id",
+  tablePartitioning: null
 });
 
 async function readFormattedSQL(file: string): Promise<string> {
