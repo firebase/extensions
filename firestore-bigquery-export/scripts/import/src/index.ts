31f615515c44fd8d2ac3e844d16a52341b884f4f
dackers86 <ackers86@hotmail.com>
2021-03-04 18:03:43 +0000

    removed console logs

--- firestore-bigquery-export/scripts/import/src/index.ts
+++ firestore-bigquery-export/scripts/import/src/index.ts
@@ -333,15 +333,6 @@ const run = async (): Promise<number> => {
 async function parseConfig(): Promise<CliConfig> {
   program.parse(process.argv);
 
-  console.warn(">>>>", program.nonInteractive);
-  console.warn(">>>>", program.project);
-  console.warn(">>>>", program.sourceCollectionPath);
-  console.warn(">>>>", program.dataset);
-  console.warn(">>>>", program.tableNamePrefix);
-  console.warn(">>>>", program.queryCollectionGroup);
-  console.warn(">>>>", program.datasetLocation);
-  console.warn(">>>>", program.batchSize);
-
   if (program.nonInteractive) {
     if (
       program.project === undefined ||
