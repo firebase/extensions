f1fead81104948db534b703180a4d0b12f31cc00
Darren Ackers <ackers86@hotmail.com>
2022-02-28 17:26:10 +0000

    feat(firestore-bigquery-export): added merge partitioning, clustering, wildcard Ids & backup collections (#890)

---
+++ firestore-bigquery-export/firestore-bigquery-change-tracker/src/__tests__/fixtures/clearTables.ts
@@ -0,0 +1,28 @@
+import { BigQuery } from "@google-cloud/bigquery";
+
+export const deleteTable = async ({
+  projectId = "extensions-testing",
+  datasetId = "",
+}) => {
+  const bq = new BigQuery({ projectId });
+  return new Promise((resolve) => {
+    const dataset = bq.dataset(datasetId);
+
+    let handle = setInterval(async () => {
+      const [datasetExists] = await dataset.exists();
+
+      if (!datasetExists) {
+        clearInterval(handle);
+        return resolve(dataset);
+      }
+
+      try {
+        if (datasetExists) {
+          await dataset.delete({ force: true });
+        }
+      } catch (ex) {
+        console.warn(`Attempted to clear ${datasetId}`, ex.message);
+      }
+    }, 500);
+  });
+};
