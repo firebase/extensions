aa683693d38b9bd4acb4039cf1b6975102c18017
Darren Ackers <ackers86@hotmail.com>
2022-03-02 16:43:58 +0000

    fix(firestore-send-email): added local smtp server for running tests (#794)
    * fix(firestore-send-email): added local smtp server for running tests
    
    * updates to local smtp config
    
    * updated testing port for smtp
    
    * updated test smtp transport switch
    
    * package json cleanup
    
    * removed env setting for auth tls
    
    * Added missing  smtp server depencency

--- firestore-send-email/functions/src/index.ts
+++ firestore-send-email/functions/src/index.ts
@@ -49,26 +49,17 @@ async function initialize() {
 
 async function transportLayer() {
   if (config.testing) {
-    return new Promise((resolve, reject) => {
-      nodemailer.createTestAccount((err, account) => {
-        if (err) {
-          reject(err);
-        }
-        const testSMTPCredentials = nodemailer.createTransport({
-          host: "smtp.ethereal.email",
-          port: 587,
-          secure: false, // true for 465, false for other ports
-          auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass, // generated ethereal password
-          },
-        });
-        resolve(testSMTPCredentials);
-      });
+    return nodemailer.createTransport({
+      host: "localhost",
+      port: 8132,
+      secure: false,
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
-  } else {
-    return setSmtpCredentials(config);
   }
+
+  return setSmtpCredentials(config);
 }
 
 function validateFieldArray(field: string, array?: string[]) {
