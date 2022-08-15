1c0f57f1deb101bcb2359fe5b4e82df25fe5a01f
dackers86 <ackers86@hotmail.com>
2022-08-15 11:02:53 +0100

    chore: fixed validation errors

--- delete-user-data/functions/__tests__/search.test.ts
+++ delete-user-data/functions/__tests__/search.test.ts
@@ -1,7 +1,11 @@
 import * as admin from "firebase-admin";
 import { UserRecord } from "firebase-admin/lib/auth/user-record";
 import { search } from "../src//search";
-import { createFirebaseUser, waitForCollectionDeletion, waitForDocumentDeletion } from "../src/helpers";
+import {
+  createFirebaseUser,
+  waitForCollectionDeletion,
+  waitForDocumentDeletion,
+} from "../src/helpers";
 import setupEnvironment from "../__tests__/helpers/setupEnvironment";
 
 const environment = {
@@ -12,8 +16,6 @@ admin.initializeApp();
 setupEnvironment();
 
 const db = admin.firestore();
-
-
 
 const generateRandomId = () => {
   var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
@@ -27,7 +29,7 @@ const generateRandomId = () => {
 
 const generateTopLevelUserCollection = async (userId) => {
   const collection = db.collection(userId);
-  await collection.add({})
+  await collection.add({});
   return collection;
 };
 
@@ -36,35 +38,37 @@ describe("search", () => {
   let rootCollection: admin.firestore.CollectionReference;
   beforeEach(async () => {
     user = await createFirebaseUser();
-    rootCollection = await  generateTopLevelUserCollection(user.uid);
+    rootCollection = await generateTopLevelUserCollection(user.uid);
   });
 
   describe("top level collections", () => {
     test("can delete is single collection named {uid}", async () => {
-      await search(user?.uid);
+      await search(user.uid);
 
-      await waitForCollectionDeletion(rootCollection)
-    }, 60000);    
+      await waitForCollectionDeletion(rootCollection);
+    }, 60000);
   });
 
   describe("top level collection documents", () => {
     test("can delete a document named {uid}", async () => {
-      const document = await db.collection(generateRandomId()).doc(user?.uid);
-      await search(user?.uid);
+      const document = await db.collection(generateRandomId()).doc(user.uid);
+      await search(user.uid);
 
       await waitForDocumentDeletion(document);
     }, 60000);
 
     test("can delete a document with a field value named {uid}", async () => {
-      const document = await db.collection(generateRandomId()).add({ test: user.uid});
-      await search(user?.uid);
+      const document = await db
+        .collection(generateRandomId())
+        .add({ test: user.uid });
+      await search(user.uid);
 
       await waitForDocumentDeletion(document);
     }, 60000);
 
     test("can check a document without any field values", async () => {
       await db.collection(generateRandomId()).add({});
-      await search(user?.uid);
+      await search(user.uid);
 
       expect(true).toBeTruthy();
     }, 60000);
@@ -72,14 +76,17 @@ describe("search", () => {
 
   describe("sub collection", () => {
     test("can delete a subcollection document named {uid}", async () => {
-      const collection = await db.collection(generateRandomId()).doc().collection(user?.uid);
+      const collection = await db
+        .collection(generateRandomId())
+        .doc()
+        .collection(user.uid);
       await collection.add({});
 
       const checkExists = await (await collection.get()).docs[0].exists;
 
       expect(checkExists).toBe(true);
 
-      await search(user?.uid);
+      await search(user.uid);
 
       await waitForCollectionDeletion(collection);
     }, 60000);
