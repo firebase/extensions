631a158116f327a2cdfd3af000895737d9836144
Yaman KATBY <m.yaman.katby@gmail.com>
2022-08-25 14:41:15 +0300

    feat(translate-collection-script): call google-translate api

--- firestore-translate-text/scripts/translate-collection/src/index.ts
+++ firestore-translate-text/scripts/translate-collection/src/index.ts
@@ -1,5 +1,7 @@
 import { program } from "commander";
 import { parseConfig } from "./config";
+import firebase from "firebase-admin";
+import googleTranslate from "@google-cloud/translate";
 
 const packageJson = require("../package.json");
 
@@ -34,4 +36,37 @@ program
 
 async function run(options: any) {
   const config = await parseConfig(options);
+  const {
+    projectId,
+    languages,
+    collectionPath,
+    inputFieldName,
+    outputFieldName,
+  } = config;
+
+  // Initialize Firebase
+  const app = firebase.initializeApp({
+    credential: firebase.credential.applicationDefault(),
+    databaseURL: `https://${projectId}.firebaseio.com`,
+  });
+
+  process.env.PROJECT_ID = projectId;
+  process.env.GOOGLE_CLOUD_PROJECT = projectId;
+
+  const db = app.firestore();
+  const collection = db.collection(collectionPath);
+  const snapshot = await collection.get();
+  const docs = snapshot.docs;
+
+  const translate = new googleTranslate.v2.Translate();
+
+  for (const doc of docs) {
+    const input = doc.get(inputFieldName);
+    try {
+      const output = await translate.translate(input, { to: languages[0] });
+      await doc.ref.update({ [outputFieldName]: output });
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
