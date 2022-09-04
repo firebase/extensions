d853df84728ee7c5ca096b0c24c73fb1f247067d
bariscankibarcekici <106708281+Bariscankibarcekici@users.noreply.github.com>
2022-09-04 07:42:02 +0300

    restore1
    RESTORE REVERT1

--- firestore-send-email/functions/src/helpers.ts
+++ firestore-send-email/functions/src/helpers.ts
@@ -3,38 +3,22 @@ import { URL } from "url";
 import { invalidURI } from "./logs";
 import { Config } from "./types";
 
-function compileUrl($: string): URL | null {
-  try {
-    return new URL($);
-  } catch (ex) {
-    return null;
-  }
-}
-
 export const setSmtpCredentials = (config: Config) => {
-  let url;
+  const { smtpConnectionUri, smtpPassword } = config;
+  let url = new URL(smtpConnectionUri);
   let transport;
 
-  const { smtpConnectionUri, smtpPassword } = config;
-
-  /** Generate Url object */
-  url = compileUrl(smtpConnectionUri);
-
-  /** return null if invalid url */
   if (!url) {
     invalidURI(smtpConnectionUri);
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 65c80ffc (fix(firestore-send-email): added encoding for connection string passwords (#985))
     return null;
   }
 
-  /** encode uri password if exists */
-  if (url.password) {
-    url.password = encodeURIComponent(url.password);
-  }
-
-  /** encode secret password if exists */
   if (url.hostname && smtpPassword) {
-    url.password = encodeURIComponent(smtpPassword);
+    url.password = smtpPassword;
   }
 
   transport = createTransport(url.href);
