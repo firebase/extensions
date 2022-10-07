29ed2aa10d507c13aec9793b2b1c9799fe05238a
dackers86 <ackers86@hotmail.com>
2022-10-07 15:27:30 +0100

    chore: renamed stoage  bucket to cloud storage bucket

--- delete-user-data/functions/src/config.ts
+++ delete-user-data/functions/src/config.ts
@@ -22,7 +22,7 @@ export default {
   storagePaths: process.env.STORAGE_PATHS,
   enableSearch: process.env.ENABLE_AUTO_DISCOVERY === "yes",
   storageBucketDefault:
-    process.env.STORAGE_BUCKET || process.env.STORAGE_BUCKET,
+    process.env.CLOUD_STORAGE_BUCKET || process.env.STORAGE_BUCKET,
   selectedDatabaseInstance: process.env.SELECTED_DATABASE_INSTANCE,
   selectedDatabaseLocation: process.env.SELECTED_DATABASE_LOCATION,
   searchFields: process.env.AUTO_DISCOVERY_SEARCH_FIELDS || "",
