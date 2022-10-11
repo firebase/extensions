404dc40f3fc3fd3af5c6742d11a9b3ae420debfe
Elliot Hesp <elliot.hesp@gmail.com>
2022-10-11 08:46:58 +0100

    chore: add license headers

--- firestore-bundle-builder/functions/src/index.ts
+++ firestore-bundle-builder/functions/src/index.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * This extension sets up a http-serving Cloud Function.
  *
@@ -8,7 +24,6 @@
 
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import { Timestamp } from "@google-cloud/firestore";
 import { BundleSpec, build, ParamsSpec } from "./build_bundle";
 import { Storage } from "@google-cloud/storage";
 import { createGzip } from "zlib";
