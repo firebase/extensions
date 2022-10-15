8b941d6a54d983412063d64046b728b8c646a71c
Darren Ackers <ackers86@hotmail.com>
2022-10-15 16:07:50 +0100

    feat(delete-user-data): add auto discovery mechanisms + update documentation
    Co-authored-by: Elliot Hesp <elliot.hesp@gmail.com>

---
+++ delete-user-data/functions/__tests__/search.test.ts
@@ -0,0 +1,165 @@
+import * as admin from "firebase-admin";
+import { UserRecord } from "firebase-admin/lib/auth/user-record";
+import { search } from "../src/search";
+import {
+  createFirebaseUser,
+  waitForCollectionDeletion,
+  waitForDocumentDeletion,
+} from "./helpers";
+import setupEnvironment from "../__tests__/helpers/setupEnvironment";
+import config from "../src/config";
+
+const environment = {
+  queryCollection: "queries",
+};
+
+admin.initializeApp();
+setupEnvironment();
+
+const db = admin.firestore();
+
+const generateRandomId = () => {
+  var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+  var string = "";
+  for (var ii = 0; ii < 15; ii++) {
+    string += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return `${string}@google.com`;
+};
+
+const generateTopLevelUserCollection = async (userId) => {
+  const collection = db.collection(userId);
+  await collection.add({});
+  return collection;
+};
+
+describe("discovery", () => {
+  let user: UserRecord;
+  let rootCollection: admin.firestore.CollectionReference;
+  beforeEach(async () => {
+    user = await createFirebaseUser();
+    rootCollection = await generateTopLevelUserCollection(user.uid);
+  });
+
+  describe("searches on top level collections", () => {
+    test("can delete is single collection named {uid}", async () => {
+      await search(user.uid, 1);
+
+      await waitForCollectionDeletion(rootCollection);
+    }, 60000);
+  });
+
+  describe("searches on top level collection documents", () => {
+    test("can delete a document named {uid}", async () => {
+      const document = await db.collection(generateRandomId()).doc(user.uid);
+      await search(user.uid, 1);
+
+      await waitForDocumentDeletion(document);
+    }, 60000);
+
+    test("can delete a document with a field value named {uid}", async () => {
+      const document = await db
+        .collection(generateRandomId())
+        .add({ field1: user.uid });
+      await search(user.uid, 1);
+
+      await waitForDocumentDeletion(document);
+    }, 60000);
+
+    test("can check a document without any field values", async () => {
+      await db.collection(generateRandomId()).add({});
+      await search(user.uid, 1);
+
+      expect(true).toBeTruthy();
+    }, 60000);
+  });
+
+  describe("sub collection", () => {
+    test("can delete a subcollection named {uid}", async () => {
+      const collection = await db.collection(
+        "can-delete-a-subcollection-named-uid"
+      );
+
+      const subcollection = await collection
+        .doc("subcollection")
+        .collection(user.uid);
+
+      await subcollection.add({ foo: "bar" });
+
+      const checkExists = await subcollection
+        .get()
+        .then((col) => col.docs.length > 0);
+
+      expect(checkExists).toBe(true);
+
+      await search(user.uid, 1);
+
+      await waitForCollectionDeletion(subcollection);
+    }, 60000);
+  });
+
+  describe("does not exceed the search depth", () => {
+    test("on a collection named {uid}", async () => {
+      const subcollection = await db
+        .collection("1")
+        .doc("1")
+        .collection("2")
+        .doc("2")
+        .collection("3")
+        .doc("3")
+        .collection("4")
+        .doc("4")
+        .collection(`${user.uid}`);
+
+      await subcollection.add({ foo: "bar" });
+
+      const collectionPathCount = subcollection.path.split("/").length / 2;
+
+      expect(collectionPathCount).toBeGreaterThan(config.searchDepth);
+
+      await search(user.uid, 1);
+
+      // /** Wait 10 seconds for the discovery to complete */
+      await new Promise((resolve) => setTimeout(resolve, 20000));
+
+      // /** Check that document still exists */
+      const checkExists = await subcollection.get().then((collection) => {
+        return collection.docs.length > 0;
+      });
+
+      expect(checkExists).toBe(true);
+    }, 60000);
+
+    test("on a document with a field named {uid}", async () => {
+      const subcollection = await db
+        .collection("1")
+        .doc("1")
+        .collection("2")
+        .doc("2")
+        .collection("3")
+        .doc("3")
+        .collection("4")
+        .doc("4")
+        .collection("5");
+
+      await subcollection.add({ field1: `${user.uid}` });
+
+      const collectionPathCount = subcollection.path.split("/").length / 2;
+
+      expect(collectionPathCount).toBeGreaterThan(config.searchDepth);
+
+      await search(user.uid, 1);
+
+      // /** Wait 10 seconds for the discovery to complete */
+      await new Promise((resolve) => setTimeout(resolve, 20000));
+
+      // /** Check that document still exists */
+      const checkExists = await subcollection.get().then((collection) => {
+        return collection.docs.length > 0;
+      });
+
+      expect(checkExists).toBe(true);
+    }, 60000);
+  });
+});
