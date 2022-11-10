e728c0d39ae25d0578cd2e5d8de89f140228fbe1
Yaman KATBY <m.yaman.katby@gmail.com>
2022-11-10 16:45:25 +0300

    fix(firestore-bigquery-export): updating table metadata too often
    Co-authored-by: dackers86 <ackers86@hotmail.com>

---
+++ firestore-bigquery-export/firestore-bigquery-change-tracker/src/bigquery/checkUpdates.ts
@@ -0,0 +1,67 @@
+import { Table, TableMetadata } from "@google-cloud/bigquery/build/src/table";
+import { Partitioning } from "./partitioning";
+
+import { FirestoreBigQueryEventHistoryTrackerConfig } from ".";
+
+export async function tableRequiresUpdate(
+  table: Table,
+  config: FirestoreBigQueryEventHistoryTrackerConfig,
+  schemaFields: any,
+  documentIdColExists: boolean,
+  pathParamsColExists: boolean
+): Promise<boolean> {
+  /* Setup checks */
+  const { metadata } = table;
+
+  /** Check clustering */
+  const configCluster = JSON.stringify(config.clustering);
+  const tableCluster = JSON.stringify(metadata.clustering?.fields || []);
+  if (configCluster !== tableCluster) return true;
+
+  /** Check wildcards */
+  const initializedWildcards = schemaFields.some(
+    ($) => $.name === "path_params"
+  ).length;
+  if (!!config.wildcardIds !== !!initializedWildcards) return true;
+
+  /** Check document id column */
+  if (!documentIdColExists) return true;
+
+  /** Checkout pathParam column exists */
+  if (!pathParamsColExists) return true;
+
+  /** Check partitioning */
+  const partitioning = new Partitioning(config, table);
+  const isValidPartition =
+    await partitioning.isValidPartitionForExistingTable();
+  if (isValidPartition) return true;
+
+  // No updates have occured.
+  return false;
+}
+
+export function viewRequiresUpdate(
+  config: FirestoreBigQueryEventHistoryTrackerConfig,
+  schemaFields: any,
+  documentIdColExists: boolean,
+  pathParamsColExists: boolean
+): boolean {
+  /** Check if documentId column exists */
+  if (!documentIdColExists) return true;
+
+  /** Check wildcards */
+  const initializedWildcards = schemaFields.some(
+    ($) => $.name === "path_params"
+  ).length;
+
+  if (!!config.wildcardIds !== !!initializedWildcards) return true;
+
+  /** Check document id column */
+  if (!documentIdColExists) return true;
+
+  /** Checkout pathParam column exists */
+  if (!pathParamsColExists) return true;
+
+  // No updates have occured.
+  return false;
+}
