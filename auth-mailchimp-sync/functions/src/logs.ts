bbfe80462d0f0a2e622e282ac6295fbe19248e18
joehan <joehanley@google.com>
2022-11-22 11:36:32 -0800

    Update auth-mailchimp-sync/functions/src/logs.ts
    Co-authored-by: Jacob Cable <32874567+cabljac@users.noreply.github.com>

--- auth-mailchimp-sync/functions/src/logs.ts
+++ auth-mailchimp-sync/functions/src/logs.ts
@@ -96,6 +96,6 @@ export const userRemoving = (
 
 export const backfillComplete = (successCount: number, errorCount: number) => {
   logger.log(
-    `Finshed adding existing users to Mailchimp audience. ${successCount} users added, ${errorCount} errors.`
+    `Finished adding existing users to Mailchimp audience. ${successCount} users added, ${errorCount} errors.`
   );
 };
