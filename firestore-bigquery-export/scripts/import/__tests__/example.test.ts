bd8cde585c04773bf5980a3294a265631bff9f18
Jacob Cable <jacobcable94@gmail.com>
2022-11-30 14:38:21 +0000

    fix(firestore-bigquery-export): match collection group queries correctly

--- firestore-bigquery-export/scripts/import/__tests__/example.test.ts
+++ firestore-bigquery-export/scripts/import/__tests__/example.test.ts
@@ -10,7 +10,7 @@ const projectId = "extensions-testing";
 
 const bigquery = new BigQuery({ projectId });
 
-async function runScript(scriptPath, callback, args?: string[]) {
+async function runScript(scriptPath: string, callback, args?: string[]) {
   return new Promise<void>((resolve, reject) => {
     // keep track of whether callback has been invoked to prevent multiple invocations
     let invoked = false;
@@ -45,13 +45,13 @@ describe("CLI", () => {
   let firestore;
   let collectionName = "testCollection";
   let datasetName = "testDataset";
-  const tableName = "testTable";
-
-  const randomID = Math.random().toString(36).substring(7);
+  let tableName = "testTable";
 
   beforeEach(async () => {
+    const randomID = Math.random().toString(36).substring(7);
     collectionName = `testCollection_${randomID}`;
     datasetName = `testDataset_${randomID}`;
+    tableName = `testTable_${randomID}`;
 
     //This is live config, should be emulator?
     process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
@@ -78,9 +78,9 @@ describe("CLI", () => {
 
   afterEach(async () => {
     // if dataset exists, delete it
-    if ((await bigquery.dataset(datasetName).exists())[0]) {
-      await bigquery.dataset(datasetName).delete({ force: true });
-    }
+    // if ((await bigquery.dataset(datasetName).exists())[0]) {
+    //   await bigquery.dataset(datasetName).delete({ force: true });
+    // }
   });
 
   test(`should import data with old script`, async () => {
@@ -117,9 +117,9 @@ describe("CLI", () => {
           .dataset(datasetName)
           .table(`${tableName}_raw_changelog`)
           .getRows(),
-      (rows) => rows.length > 0,
+      (rows) => rows[0].length > 0,
       10,
-      8000
+      20000
     );
 
     const {
@@ -131,7 +131,6 @@ describe("CLI", () => {
       event_id,
       old_data,
     } = rows[0];
-    console.log(rows[0]);
 
     expect(operation).toBe("IMPORT");
     expect(document_name).toBe(
@@ -179,11 +178,12 @@ describe("CLI", () => {
           .dataset(datasetName)
           .table(`${tableName}_raw_changelog`)
           .getRows(),
-      (rows) => rows.length > 0,
+      (rows) => rows[0].length > 0,
       10,
-      8000
+      20000
     );
 
+    expect(rows.length).toBeGreaterThanOrEqual(1);
     const {
       operation,
       timestamp,
@@ -200,6 +200,7 @@ describe("CLI", () => {
     );
     expect(document_id).toBe("test");
     expect(JSON.parse(data)).toEqual({ test: "test" });
+
     expect(event_id).toBe("");
     expect(old_data).toBeNull();
     expect(timestamp).toBeDefined();
@@ -213,5 +214,294 @@ describe("CLI", () => {
     expect(query).toBeDefined();
     const isOldQuery = query.includes("FIRST_VALUE");
     expect(isOldQuery).toBe(false);
+  });
+
+  test("collection group query", async () => {
+    process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+    process.env.FIREBASE_CONFIG = JSON.stringify({
+      apiKey: "AIzaSyAJTgFI-OVRjgd_10JDWc9T3kxvxY-fUe4",
+      authDomain: "extensions-testing.firebaseapp.com",
+      databaseURL: "https://extensions-testing.firebaseio.com",
+      projectId: "extensions-testing",
+      storageBucket: "extensions-testing.appspot.com",
+      messagingSenderId: "219368645393",
+      appId: "1:219368645393:web:e92083eba0c53f366862b0",
+      measurementId: "G-QF38ZM1SZN",
+    });
+    // if there is no app, initialize one
+    if (!admin.apps.length) {
+      admin.initializeApp();
+    }
+    firestore = admin.firestore();
+
+    await firestore
+      .collection("regions/europe/countries")
+      .doc("france")
+      .set({ name: "France" });
+
+    await firestore
+      .collection("regions/asia/countries")
+      .doc("japan")
+      .set({ name: "Japan" });
+
+    console.log("here");
+
+    const args = [
+      "--non-interactive",
+      "-P",
+      "extensions-testing",
+      "-u",
+      "-s",
+      "regions/{regionId}/countries",
+      "-d",
+      datasetName,
+      "-t",
+      tableName,
+      "-q",
+      "true",
+      "-l",
+      "us",
+      "-u",
+      "true",
+      "-e",
+      "true",
+    ];
+
+    await runScript(
+      scriptPath,
+      () => {
+        console.log("complete!");
+      },
+      args
+    );
+
+    console.log(datasetName);
+
+    const [rows] = await repeat(
+      () =>
+        bigquery
+          .dataset(datasetName)
+          .table(`${tableName}_raw_changelog`)
+          .getRows(),
+      (rows) => rows[0].length > 1,
+      10,
+      20000
+    );
+
+    expect(rows.length).toBe(2);
+    const {
+      operation,
+      timestamp,
+      document_name,
+      document_id,
+      data,
+      event_id,
+      old_data,
+    } = rows[0];
+
+    expect(operation).toBe("IMPORT");
+    // expect(document_name).toBe(
+    //   `projects/extensions-testing/databases/(default)/documents/${collectionName}/test`
+    // );
+    // expect(document_id).toBe("test");
+    // expect(JSON.parse(data)).toEqual({ test: "test" });
+
+    expect(event_id).toBe("");
+    expect(old_data).toBeNull();
+    expect(timestamp).toBeDefined();
+
+    const [view] = await bigquery
+      .dataset(datasetName)
+      .table(`${tableName}_raw_latest`)
+      .get();
+
+    const query = view.metadata.view.query;
+    expect(query).toBeDefined();
+    const isOldQuery = query.includes("FIRST_VALUE");
+    expect(isOldQuery).toBe(false);
+  });
+  test("shouldn't export non-matching results from collection group query", async () => {
+    process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+    process.env.FIREBASE_CONFIG = JSON.stringify({
+      apiKey: "AIzaSyAJTgFI-OVRjgd_10JDWc9T3kxvxY-fUe4",
+      authDomain: "extensions-testing.firebaseapp.com",
+      databaseURL: "https://extensions-testing.firebaseio.com",
+      projectId: "extensions-testing",
+      storageBucket: "extensions-testing.appspot.com",
+      messagingSenderId: "219368645393",
+      appId: "1:219368645393:web:e92083eba0c53f366862b0",
+      measurementId: "G-QF38ZM1SZN",
+    });
+    // if there is no app, initialize one
+    if (!admin.apps.length) {
+      admin.initializeApp();
+    }
+    firestore = admin.firestore();
+
+    await firestore
+      .collection("regions/europe/countries")
+      .doc("france")
+      .set({ name: "France" });
+
+    await firestore
+      .collection("regions/asia/countries")
+      .doc("japan")
+      .set({ name: "Japan" });
+
+    await firestore
+      .collection("notregions/asia/countries")
+      .doc("foo")
+      .set({ name: "Bar" });
+
+    console.log("here");
+
+    const args = [
+      "--non-interactive",
+      "-P",
+      "extensions-testing",
+      "-u",
+      "-s",
+      "regions/{regionId}/countries",
+      "-d",
+      datasetName,
+      "-t",
+      tableName,
+      "-q",
+      "true",
+      "-l",
+      "us",
+      "-u",
+      "true",
+      "-e",
+      "true",
+    ];
+
+    await runScript(
+      scriptPath,
+      () => {
+        console.log("complete!");
+      },
+      args
+    );
+
+    console.log(datasetName);
+
+    const [rows] = await repeat(
+      () =>
+        bigquery
+          .dataset(datasetName)
+          .table(`${tableName}_raw_changelog`)
+          .getRows(),
+      (rows) => rows[0].length > 1,
+      10,
+      20000
+    );
+
+    expect(rows.length).toBe(2);
+    const {
+      operation,
+      timestamp,
+      document_name,
+      document_id,
+      data,
+      event_id,
+      old_data,
+    } = rows[0];
+
+    expect(operation).toBe("IMPORT");
+    // expect(document_name).toBe(
+    //   `projects/extensions-testing/databases/(default)/documents/${collectionName}/test`
+    // );
+    // expect(document_id).toBe("test");
+    // expect(JSON.parse(data)).toEqual({ test: "test" });
+
+    expect(event_id).toBe("");
+    expect(old_data).toBeNull();
+    expect(timestamp).toBeDefined();
+
+    const [view] = await bigquery
+      .dataset(datasetName)
+      .table(`${tableName}_raw_latest`)
+      .get();
+
+    const query = view.metadata.view.query;
+    expect(query).toBeDefined();
+    const isOldQuery = query.includes("FIRST_VALUE");
+    expect(isOldQuery).toBe(false);
+  });
+
+  test("should match several wildcards in one query", async () => {
+    process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+    process.env.FIREBASE_CONFIG = JSON.stringify({
+      apiKey: "AIzaSyAJTgFI-OVRjgd_10JDWc9T3kxvxY-fUe4",
+      authDomain: "extensions-testing.firebaseapp.com",
+      databaseURL: "https://extensions-testing.firebaseio.com",
+      projectId: "extensions-testing",
+      storageBucket: "extensions-testing.appspot.com",
+      messagingSenderId: "219368645393",
+      appId: "1:219368645393:web:e92083eba0c53f366862b0",
+      measurementId: "G-QF38ZM1SZN",
+    });
+    // if there is no app, initialize one
+    if (!admin.apps.length) {
+      admin.initializeApp();
+    }
+    firestore = admin.firestore();
+
+    await firestore
+      .collection("regions/europe/countries/france/cities")
+      .doc("paris")
+      .set({ name: "Paris" });
+
+    const args = [
+      "--non-interactive",
+      "-P",
+      "extensions-testing",
+      "-u",
+      "-s",
+      "regions/{regionId}/countries/{countryId}/cities",
+      "-d",
+      datasetName,
+      "-t",
+      tableName,
+      "-q",
+      "true",
+      "-l",
+      "us",
+      "-u",
+      "true",
+      "-e",
+      "true",
+    ];
+
+    await runScript(
+      scriptPath,
+      () => {
+        console.log("complete!");
+      },
+      args
+    );
+
+    const [rows] = await repeat(
+      () =>
+        bigquery
+          .dataset(datasetName)
+          .table(`${tableName}_raw_changelog`)
+          .getRows(),
+      (rows) => rows[0].length >= 1,
+      10,
+      20000
+    );
+
+    expect(rows.length).toBe(1);
+    console.log(rows[0]);
+    const { path_params } = rows[0];
+
+    const pathParams = JSON.parse(path_params);
+
+    expect(pathParams).toHaveProperty("regionId");
+    expect(pathParams).toHaveProperty("countryId");
+    expect(pathParams.regionId).toBe("europe");
+    expect(pathParams.countryId).toBe("france");
   });
 });
