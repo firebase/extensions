f948150cd28a97a8d7e52b0da9c17eb5dc7d2cd2
pr_Mais <mais.alheraki@gmail.com>
2023-01-03 16:05:45 +0300

    chore: formatting

--- firestore-counter/clients/dart/distributed_counter.dart
+++ firestore-counter/clients/dart/distributed_counter.dart
@@ -1,22 +1,16 @@
 import 'package:async/async.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:uuid/uuid.dart';
+
 const SHARD_COLLECTION_ID = '_counter_shards_';
 
 class DistributedCounter {
-
-  /// Constructs a sharded counter object that references to a field
-  /// in a document that is a counter.
-  ///
-  /// @param doc A reference to a document with a counter field.
-  /// @param field A path to a counter field in the above document.
-  DistributedCounter(
-    this.doc,
-    this.field,
-  ) {
+  /// Constructs a sharded counter object that references to a [field]
+  /// in a [document] that is a counter.
+  DistributedCounter(this.document, this.field) {
     this.shardId = Uuid().v4();
-    final shardsRef = doc.collection(SHARD_COLLECTION_ID);
-    shards[doc.path] = 0;
+    final shardsRef = document.collection(SHARD_COLLECTION_ID);
+    shards[document.path] = 0;
     shards[shardsRef.doc(shardId).path] = 0;
     shards[shardsRef.doc('\t' + shardId.substring(0, 4)).path] = 0;
     shards[shardsRef.doc('\t\t' + shardId.substring(0, 3)).path] = 0;
@@ -24,31 +18,28 @@ class DistributedCounter {
     shards[shardsRef.doc('\t\t\t\t' + shardId.substring(0, 1)).path] = 0;
   }
 
-  late final String shardId;
-
-  final DocumentReference<Object?> doc;
+  final DocumentReference<Object?> document;
   final String field;
   final Map<String, int> shards = {};
-  final Stream<int> _snapshots;
+  late final String shardId;
 
   /// Get latency compensated view of the counter.
   ///
   /// All local increments will be reflected in the counter even if the main
   /// counter hasn't been updated yet.
- Future<int> get(
-      {GetOptions options =
-          const GetOptions(source: Source.server)}) async {
-    final valuePromises = shards.keys.map((path) async {
-      final shard = await doc.firestore.doc(path).get(options);
+  Future<int> get({
+    GetOptions options = const GetOptions(source: Source.server),
+  }) async {
+    final shardFutures = shards.keys.map((path) async {
+      final shard = await document.firestore.doc(path).get(options);
       final shardData = shard.data();
       if (shardData != null && shardData.containsKey(field)) {
         return shard.get(field) ?? 0;
-      }
-      else{
+      } else {
         return 0;
       }
     });
-    final values = await Future.wait(valuePromises);
+    final values = await Future.wait(shardFutures);
     return values.reduce((a, b) => a + b);
   }
 
@@ -57,25 +48,32 @@ class DistributedCounter {
   /// All local increments to this counter will be immediately visible in the
   /// snapshot.
   Stream<int> onSnapshot() {
-    return StreamGroup.mergeBroadcast<int>(shards.keys.map((path) =>
-        doc.firestore.doc(path).snapshots().map<int>((DocumentSnapshot snap) {
-          shards[snap.reference.path] = snap.exists ? snap.get(field) : 0;
-          return shards.values.reduce((a, b) => a + b);
-        })));
+    return StreamGroup.mergeBroadcast<int>(
+      shards.keys.map(
+        (path) => document.firestore.doc(path).snapshots().map<int>(
+          (DocumentSnapshot snap) {
+            shards[snap.reference.path] = snap.exists ? snap.get(field) : 0;
+            return shards.values.reduce((a, b) => a + b);
+          },
+        ),
+      ),
+    );
   }
 
   /// Increment the counter by a given value.
   ///
-  /// e.g.
+  /// ```dart
   /// final counter = DistributedCounter(db.doc('path/document'), 'counter');
   /// counter.incrementBy(1);
+  /// ```
   Future<void> incrementBy(int val) {
     final increment = FieldValue.increment(val);
     final toFold = this.field.split('.').reversed;
-    Map<String, dynamic> update = toFold
-        .skip(1)
-        .fold({toFold.elementAt(0): increment}, (value, name) => {name: value});
-    return doc
+    Map<String, dynamic> update = toFold.skip(1).fold(
+      {toFold.elementAt(0): increment},
+      (value, name) => {name: value},
+    );
+    return document
         .collection(SHARD_COLLECTION_ID)
         .doc(shardId)
         .set(update, SetOptions(merge: true));
