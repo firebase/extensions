2dccdcde6958ed9c688b282a8f0905f17aa16fc7
Jacob Cable <jacobcable94@gmail.com>
2023-01-19 10:25:50 +0000

    chore(*): lint and format

--- storage-resize-images/functions/src/index.ts
+++ storage-resize-images/functions/src/index.ts
@@ -49,8 +49,9 @@ logs.init();
  * When an image is uploaded in the Storage bucket, we generate a resized image automatically using
  * the Sharp image converting library.
  */
-export const generateResizedImage = functions.storage.object().onFinalize(
-  async (object): Promise<void> => {
+export const generateResizedImage = functions.storage
+  .object()
+  .onFinalize(async (object): Promise<void> => {
     logs.start();
     const { contentType } = object; // This is the image MIME type
 
@@ -199,5 +200,4 @@ export const generateResizedImage = functions.storage.object().onFinalize(
         }
       }
     }
-  }
-);
+  });
