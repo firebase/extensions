211e89284492dbfe5f1280bd28c4f49457517705
dackers86 <ackers86@hotmail.com>
2023-02-15 09:38:57 +0000

    fix(delete-user-data): Increase UID checks on search based deletions

--- delete-user-data/functions/src/helpers.ts
+++ delete-user-data/functions/src/helpers.ts
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+import { DocumentReference, FieldPath } from "firebase-admin/firestore";
+import config from "./config";
+
 export const getDatabaseUrl = (
   selectedDatabaseInstance: string | undefined,
   selectedDatabaseLocation: string | undefined
@@ -24,4 +27,27 @@ export const getDatabaseUrl = (
     return `https://${selectedDatabaseInstance}.firebaseio.com`;
 
   return `https://${selectedDatabaseInstance}.${selectedDatabaseLocation}.firebasedatabase.app`;
+};
+
+export const hasValidUserPath = async (
+  ref: DocumentReference,
+  path: string,
+  uid: string
+): Promise<boolean> => {
+  /** Check path for valid user id */
+  if (path.includes(uid)) return Promise.resolve(true);
+
+  /** Check to find valid field */
+  const snapshot = await ref.get();
+
+  if (snapshot.exists) {
+    for (const field of config.searchFields.split(",")) {
+      if (snapshot.get(new FieldPath(field)) === uid) {
+        return Promise.resolve(true);
+      }
+    }
+  }
+
+  /** Return as invalid path */
+  return Promise.resolve(false);
 };
