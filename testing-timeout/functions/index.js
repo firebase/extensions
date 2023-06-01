2159afd9f035a9e43c1ef785c643a6f83679dacf
Rafer45 <reao@google.com>
2023-06-01 16:25:53 -0400

    fix bads

--- testing-timeout/functions/index.js
+++ testing-timeout/functions/index.js
@@ -33,41 +33,44 @@ exports.greetTheWorld = functions.handler.https.onRequest(async (req, res) => {
   res.send(greeting);
 });
 
-exports.backfillGreetTheWorld = functions.tasks.taskQueue.onDispatch(async (data) => {
-  const MAX_GENERATION = 5
-  const runtime = getExtensions().runtime();
+exports.backfillGreetTheWorld = functions.tasks
+  .taskQueue()
+  .onDispatch(async (data) => {
+    const MAX_GENERATION = 5
+    const runtime = getExtensions().runtime();
 
-  // Here we reference a user-provided parameter (its value is provided by the user during installation)
-  const consumerProvidedGreeting = process.env.GREETING;
+    // Here we reference a user-provided parameter (its value is provided by the user during installation)
+    const consumerProvidedGreeting = process.env.GREETING;
 
-  const generation = (data && data.generation) || 0
-  // And here we reference an auto-populated parameter (its value is provided by Firebase after installation)
-  const instanceId = process.env.EXT_INSTANCE_ID;
+    const generation = (data && data.generation) || 0
+    // And here we reference an auto-populated parameter (its value is provided by Firebase after installation)
+    const instanceId = process.env.EXT_INSTANCE_ID;
 
-  const greeting = `${consumerProvidedGreeting} World from ${instanceId}`;
+    const greeting = `${consumerProvidedGreeting} World from ${instanceId}`;
 
-  for (let i = 0; i < 11*60; i++) {
-    logger.log(`B${generation}!!: Starting loop iteration number ${i}`);
-    await new Promise(r => setTimeout(r, 1000));
-  }
-  logger.log("Bafter the loop");
-  logger.log(greeting);
+    for (let i = 0; i < 11*60; i++) {
+      logger.log(`B${generation}!!: Starting loop iteration number ${i}`);
+      await new Promise(r => setTimeout(r, 1000));
+    }
+    logger.log("Bafter the loop");
+    logger.log(greeting);
 
 
-  if (generation == MAX_GENERATION) {
-    await runtime.setProcessingState(
-      "PROCESSING_COMPLETE",
-      "Bdone greeting godot"
+    if (generation == MAX_GENERATION) {
+      await runtime.setProcessingState(
+        "PROCESSING_COMPLETE",
+        "Bdone greeting godot"
+      );
+
+      return;
+    }
+
+    const queue = getFunctions().taskQueue(
+      `locations/${config.location}/functions/backfillGreetTheWorld`,
+      process.env.EXT_INSTANCE_ID
     );
-    return
-  }
 
-  const queue = getFunctions().taskQueue(
-    `locations/${config.location}/functions/backfillGreetTheWorld`,
-    process.env.EXT_INSTANCE_ID
-  );
-
-  await queue.enqueue({
-    generation: generation + 1,
-  });
+    await queue.enqueue({
+      generation: generation + 1,
+    });
 });
