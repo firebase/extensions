c7d337c9cfedd15051e1ab32d0bcccdb9192f3a9
Jacob Cable <jacobcable94@gmail.com>
2023-11-16 14:45:26 +0000

    test(firestore-translate-text): add test to demonstrate bug

--- firestore-translate-text/functions/__tests__/unit/translateMultipleBackfill.test.ts
+++ firestore-translate-text/functions/__tests__/unit/translateMultipleBackfill.test.ts
@@ -1,6 +1,11 @@
 import * as admin from "firebase-admin";
 import * as fft from "firebase-functions-test";
-import { translateMultipleBackfill } from "../../src/translate/translateMultiple";
+import {
+  translateMultiple,
+  translateMultipleBackfill,
+} from "../../src/translate/translateMultiple";
+import { updateTranslations } from "../../src/translate/common";
+const languages = ["en", "es", "fr"];
 
 jest.mock("../../src/config", () => ({
   default: {
@@ -11,18 +16,20 @@ jest.mock("../../src/config", () => ({
 
 const testFft = fft();
 
-// Mock your translateString function if it's a separate module
 jest.mock("../../src/translate/common", () => ({
   ...jest.requireActual("../../src/translate/common"),
-  translateString: jest
-    .fn()
-    .mockImplementation((text, language) =>
-      Promise.resolve(`Translated ${text} to ${language}`)
-    ),
+  updateTranslations: jest.fn().mockImplementation((snap, translations) => {
+    console.log(translations);
+  }),
+  translateString: jest.fn().mockImplementation((string, language) => {
+    return Promise.resolve(
+      `mock translated string \"${string}\" in ${language}`
+    );
+  }),
 }));
 
 const mockBulkWriterUpdate = jest.fn().mockImplementation((args) => {
-  console.timeLog(args);
+  console.log(args);
 });
 
 describe("translateMultipleBackfill", () => {
@@ -47,6 +54,58 @@ describe("translateMultipleBackfill", () => {
     );
 
     await translateMultipleBackfill(input, snapshot, bulkWriter);
+
+    expect(bulkWriter.update).toBeCalledWith(snapshot.ref, "translations", {
+      "0": {
+        en: "mock translated string for hello in en",
+        es: "mock translated string for hello in es",
+        fr: "mock translated string for hello in fr",
+      },
+      "1": {
+        en: "mock translated string for how are you? in en",
+        es: "mock translated string for how are you? in es",
+        fr: "mock translated string for how are you? in fr",
+      },
+    });
+  });
+  // Add more test cases for different scenarios
+});
+
+describe("translateMultiple", () => {
+  // Setup common variables if needed
+  let input;
+  let snapshot;
+  let bulkWriter = {
+    update: mockBulkWriterUpdate,
+  } as unknown as admin.firestore.BulkWriter;
+
+  beforeEach(() => {
+    // Reset mocks and setup common test variables
+    jest.clearAllMocks();
+  });
+
+  test("should handle valid input correctly", async () => {
+    input = ["hello", "how are you?"];
+
+    snapshot = testFft.firestore.makeDocumentSnapshot(
+      { foo: "bar" },
+      "document/path"
+    );
+
+    await translateMultiple(input, languages, snapshot);
+
+    expect(updateTranslations).toBeCalledWith(snapshot, {
+      "0": {
+        en: 'mock translated string "hello" in en',
+        es: 'mock translated string "hello" in es',
+        fr: 'mock translated string "hello" in fr',
+      },
+      "1": {
+        en: 'mock translated string "how are you?" in en',
+        es: 'mock translated string "how are you?" in es',
+        fr: 'mock translated string "how are you?" in fr',
+      },
+    });
   });
   // Add more test cases for different scenarios
 });
