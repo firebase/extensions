985fa0bef76c8743a6b5356013dc63f75b7c6ab2
Pavel <pavel@invertase.io>
2024-04-15 16:45:18 -0700

    chore: update jest configuration and dependencies

--- firestore-bigquery-export/functions/jest.config.js
+++ firestore-bigquery-export/functions/jest.config.js
@@ -1,6 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
 const packageJson = require("./package.json");
 
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   name: packageJson.name,
   displayName: packageJson.name,
   rootDir: "./",
