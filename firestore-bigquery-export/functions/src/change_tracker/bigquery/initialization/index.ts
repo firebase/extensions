62d46df0a1b044e62c9d6b16a63a221965963eac
Jacob Cable <jacobcable94@gmail.com>
2024-04-18 11:31:18 +0100

    chore(firestore-bigquery-export): remove commented code

--- firestore-bigquery-export/functions/src/change_tracker/bigquery/initialization/index.ts
+++ firestore-bigquery-export/functions/src/change_tracker/bigquery/initialization/index.ts
@@ -1,7 +1,3 @@
-// import { initializeRawChangelogTable } from "./rawChangelogTable/initializeRawChangeLogTable.js";
-// import { initializeDataset } from "./dataset/initializeDataset.js";
-// import { waitForInitialization } from "./utils";
-
 export { initializeRawChangelogTable } from "./rawChangelogTable/initializeRawChangeLogTable.js";
 export { initializeDataset } from "./dataset/initializeDataset.js";
 export { waitForInitialization } from "./utils.js";
