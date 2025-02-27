32cc2c91716239782e197621b44d471af87d9c22
Darren Ackers <ackers86@hotmail.com>
2025-02-27 16:53:31 +0000

    feat(firestore-send-email): added oauth2 (#2301)
    * feat(firestore-send-email): added oauth2
    
    * chore: added secure flag
    
    * refactor(firestore-send-email): remove unec. params
    
    * test(firestore-send-email): fix tests
    
    * chore(firestore-send-email): fix handlebars type
    
    * docs(firestore-send-email): fix oauth guide
    
    * docs(firestore-send-email): fix OAuth2 param descriptions
    
    * docs(firestore-send-email): mention secure connection
    
    * chore(firestore-send-email): bump extension version
    
    * docs(firestore-send-email): update oauth guide
    
    * chore(firestore-send-email): clean up script and remove logs
    
    ---------
    
    Co-authored-by: Jacob Cable <jacobcable94@gmail.com>

--- firestore-send-email/functions/__tests__/config.test.ts
+++ firestore-send-email/functions/__tests__/config.test.ts
@@ -1,6 +1,6 @@
 import * as functionsTestInit from "firebase-functions-test";
 import mockedEnv from "mocked-env";
-import { Config } from "../src/types";
+import { AuthenticatonType, Config } from "../src/types";
 
 //@ts-ignore
 const { config } = global;
@@ -23,6 +23,13 @@ const environment = {
   TTL_EXPIRE_TYPE: "day",
   TTL_EXPIRE_VALUE: "1",
   TLS_OPTIONS: "{}",
+  AUTH_TYPE: AuthenticatonType.OAuth2,
+  CLIENT_ID: "fake-client-id",
+  CLIENT_SECRET: "fake-client",
+  REFRESH_TOKEN: "test-refresh-token",
+  ACCESS_TOKEN: "test-access",
+  USER: "test@test.com",
+  OAUTH_PORT: "465",
 };
 
 describe("extensions config", () => {
@@ -45,6 +52,14 @@ describe("extensions config", () => {
       TTLExpireType: process.env.TTL_EXPIRE_TYPE,
       TTLExpireValue: parseInt(process.env.TTL_EXPIRE_VALUE),
       tls: process.env.TLS_OPTIONS,
+      host: process.env.HOST,
+      port: parseInt(process.env.OAUTH_PORT, null),
+      secure: process.env.SECURE === "true",
+      authenticationType: process.env.AUTH_TYPE as AuthenticatonType,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      user: process.env.USER,
     };
     const functionsConfig = config();
     expect(functionsConfig).toStrictEqual(testConfig);
