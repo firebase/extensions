a23573e0febb43fb983945583623d37b6cc9f14b
Joe Hanley <joehanley@google.com>
2026-03-17 09:52:15 -0700

    Updating to a newer version of node

--- .github/workflows/scripts/npm_publish.sh
+++ .github/workflows/scripts/npm_publish.sh
@@ -63,4 +63,6 @@ if npm_package_version_exists "$NPM_PACKAGE_NAME" "$NPM_PACKAGE_VERSION"; then
 fi
 
 echo "Attempting to publish $NPM_PACKAGE_NAME version $NPM_PACKAGE_VERSION..."
+npm --version
+node --version
 npm publish --provenance
