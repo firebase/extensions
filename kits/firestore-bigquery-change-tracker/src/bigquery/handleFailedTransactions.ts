7b782eeada09e7536e1ee042a5bec439c1ac900e
Corie Watson <watson.corie@gmail.com>
2026-07-23 16:36:20 +0100

    feat(kits): add BigQuery change tracker

---
+++ kits/firestore-bigquery-change-tracker/src/bigquery/handleFailedTransactions.ts
@@ -0,0 +1,46 @@
+import { getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import type { ChangeTrackerConfig } from ".";
+
+if (getApps().length === 0) {
+  initializeApp();
+}
+
+export default async (
+  rows: any[],
+  config: ChangeTrackerConfig,
+  e: Error
+): Promise<void> => {
+  const db = getFirestore(config.firestoreInstanceId!);
+  db.settings({
+    ignoreUndefinedProperties: true,
+  });
+  const batchArray = [db.batch()];
+
+  let operationCounter = 0;
+  let batchIndex = 0;
+
+  rows?.forEach((row) => {
+    var ref = db.collection(config.backupTableId).doc(row.insertId);
+
+    batchArray[batchIndex].set(ref, {
+      ...row,
+      error_details: e.message,
+    });
+
+    operationCounter++;
+
+    // Check if max limit for batch has been met.
+    if (operationCounter === 499) {
+      batchArray.push(db.batch());
+      batchIndex++;
+      operationCounter = 0;
+    }
+  });
+
+  for (const batch of batchArray) {
+    await batch.commit();
+  }
+
+  return Promise.resolve();
+};
