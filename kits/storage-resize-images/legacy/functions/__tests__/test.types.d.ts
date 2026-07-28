58b03ede4360b9b467befa987671e8c5f83b4b20
Corie Watson <watson.corie@gmail.com>
2026-07-28 12:45:03 +0100

    chore(kits): remove image resize legacy

--- kits/storage-resize-images/legacy/functions/__tests__/test.types.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-  interface Global {
-    config: () => jest.ModuleMocker;
-    deleteImage: () => jest.ModuleMocker;
-    mockGenerateResizedImage: () => jest.MockedFunction<any>;
-  }
-}
