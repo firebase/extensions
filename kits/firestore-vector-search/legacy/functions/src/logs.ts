e996d138da6871e725da882ae61d1c648a0705a9
Corie Watson <watson.corie@gmail.com>
2026-08-05 13:06:32 +0100

    chore(kits): remove remaining legacy folders

--- kits/firestore-vector-search/legacy/functions/src/logs.ts
+++
@@ -1,6 +0,0 @@
-import {obfuscatedConfig} from './config';
-import {logger} from 'firebase-functions/v1';
-
-export function init() {
-  logger.log('Initializing extension with configuration', obfuscatedConfig);
-}
--- kits/storage-resize-images/legacy/functions/__tests__/__mocks__/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as functionsTestInit from "firebase-functions-test";
-import { generateResizedImage as originalGenerateResizedImage } from "../../../src";
-
-export const generateResizedImage = () => {
-  let functionsTest = functionsTestInit();
-  return functionsTest.wrap(originalGenerateResizedImage);
-};
